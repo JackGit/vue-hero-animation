fb74ea261bee6b9675f2786e00a900aa82b45165
jack <ygjack414@hotmail.com>
2016-05-30 17:58:05 +0800

    init vue-hero-animation.js

--- test/demo.js
+++ test/demo.js
@@ -4,9 +4,9 @@ var router = new VueRouter();
 var PageA = Vue.extend({
     template: '#pageATemplate',
     methods: {
-        toPageB: function() {
+        toPageB: function(value) {
             "use strict";
-            this.$router.go({name: 'pageB'})
+            this.$router.go({name: 'pageB', params: {value: value}})
         }
     }
 });
@@ -21,120 +21,17 @@ var PageB = Vue.extend({
     }
 });
 
-var App = Vue.extend({
-
-});
-
-var heroName = 'myHero';
-var clonedHeroElement;
-var getHeroElements = function(heroName) {
-    "use strict";
-    return document.querySelectorAll('[hero-name="' + heroName + '"]');
-}
-var cloneHero = function(heroElement) {
-    "use strict";
-    var element = heroElement.cloneNode(true);
-    var rect = heroElement.getBoundingClientRect();
-
-    element.style.position = 'fixed';
-    element.style.top = rect.top + 'px';
-    element.style.left = rect.left + 'px';
-    element.style.height = rect.height + 'px';
-    element.style.width = rect.width + 'px';
-    element.style.transition = 'all .3s ease';
-    element.style.willChange = 'auto';
-
-    document.body.appendChild(element);
-    heroElement.style.opacity = 0;
-
-    return element;
-}
-
-Vue.transition('fade', {
-
-    beforeEnter: function () {
-        console.log('before enter', this)
-    },
-    enter: function () {
-        // if there is a source hero element
-        // and there is a destination hero element,
-        // then apply below code
-        // otherwise, return
-
-        console.log('enter')
-        // set opacity = 0 of the heroes in the coming page
-        var hero = getHeroElements(heroName)[0];
+var App = Vue.extend({});
 
 
-        var sourceRect = clonedHeroElement.getBoundingClientRect();
-        var destRect = hero.getBoundingClientRect();
-        var sourceWidth = sourceRect.width;
-        var sourceHeight = sourceRect.height;
-        var sourceCenter = {x: sourceRect.left + sourceWidth / 2, y: sourceRect.top + sourceHeight / 2};
-        var destWidth = destRect.width;
-        var destHeight = destRect.height;
-        var destCenter = {x: destRect.left + destWidth / 2, y: destRect.top + destHeight / 2};
-
-        hero.style.opacity = 0;
-
-        var newStyle = getComputedStyle(hero);
-
-        clonedHeroElement.style.borderRadius = newStyle.borderRadius;
-        clonedHeroElement.style.background = newStyle.background;
-        clonedHeroElement.style.width = newStyle.width;
-        clonedHeroElement.style.height = newStyle.height;
-        clonedHeroElement.style.fontSize = newStyle.fontSize;
-        clonedHeroElement.style.padding = newStyle.padding;
-
-        var stopped = false;
-
-        clonedHeroElement.addEventListener('transitionend', function() {
-            "use strict";
-            if(stopped)
-                return;
-            hero.style.opacity = 1;
-            clonedHeroElement.remove();
-        });
-
-
-        // FLIP.last(); make a snapshot as last position
-        // FLIP.play(); to play cloned hero from the first position to the last position
-        // and when the hero animation completed, remove the cloned heroes, and set original heroes opacity = 1
-
-    },
-    afterEnter: function () {
-        console.log('after enter')
-    },
-    enterCancelled: function () {
-
-    },
-
-    beforeLeave: function () {
-        // clone the heroes in current page
-        var hero = getHeroElements(heroName)[0];
-
-        clonedHeroElement = cloneHero(hero);
-
-
-        // FLIP.first(); make a snapshot as first position
-    },
-    leave: function () {
-
-    },
-    afterLeave: function () {
-
-    },
-    leaveCancelled: function (el) {
-        // handle cancellation
-    }
-})
+VueAnimatedList.rescue({fade: ['hero-avatar', 'hero-name', 'hero-page']})
 
 router.map({
     '/pageA': {
         name: 'pageA',
         component: PageA
     },
-    '/pageB': {
+    '/pageB/': {
         name: 'pageB',
         component: PageB
     }
--- vue-hero-animation.js
+++ vue-hero-animation.js
@@ -2,6 +2,7 @@
 // hero-duration
 // hero-delay
 // hero-timing
+// hero-depth
 // hero => to identify this element will be used as source or destination hero element
 
 
@@ -36,14 +37,37 @@
         _init: function() {
             this._hookEnter();
             this._hookBeforeLeave();
+
+            // re-hook the transitions
+            Vue.transition(this.transitionName, this.transitionOptions);
         },
 
         _hookEnter: function() {
+            var that = this;
             var oldHook = this.transitionOptions.enter;
 
+            // for routers, target heroes have to be found right before the enter transition started
             this.transitionOptions.enter = function() {
-                oldHook && oldHook.apply(this, arguments);
+                var heroName;
 
+                oldHook && oldHook.apply(this, arguments);
+                // for every found active hero in the outgoing page, try to find its destination hero
+                for(heroName in that.mapping) {
+                    var destHero = document.querySelector('[hero-name="' + heroName + '"]');
+
+                    if(destHero) {
+                        that.mapping[heroName].dest = destHero;
+                        that.mapping[heroName].destClone = destHero.cloneNode();
+                        that.mapping[heroName].destBoundingClientRect = destHero.getBoundingClientRect();
+
+                        that.mapping[heroName].destClone.style.cssText = getComputedStyle(destHero).cssText;
+                        that.mapping[heroName].destClone.style.opacity = 0;
+                        that._placeClone(that.mapping[heroName].destClone, that.mapping[heroName].destBoundingClientRect);
+                    }
+                }
+
+                //
+                that._transit();
             };
         },
 
@@ -51,18 +75,85 @@
             var oldHook = this.transitionOptions.beforeLeave;
             var that = this;
 
+            // for routers, heroes have to be found the active heroes before the page leaves
+            // an active hero is the element with hero-name and hero attributes
+
             this.transitionOptions.beforeLeave = function() {
                 oldHook && oldHook.apply(this, arguments);
 
-                that.heroName.forEach(function(heroName) {
+                that.heroNames.forEach(function(heroName) {
+                    // only one hero can be active with the same hero name
+                    var activeHero = document.querySelector('[hero=""][hero-name="' + heroName + '"]');
+
+                    if(activeHero) {
+                        that.mapping[heroName] = {
+                            source: activeHero,
+                            sourceClone: activeHero.cloneNode(activeHero.childElementCount === 0 ? true : false),
+                            sourceBoundingClientRect: activeHero.getBoundingClientRect()
+                        };
+
+                        that.mapping[heroName].sourceClone.style.cssText = getComputedStyle(activeHero).cssText;
+                        that._placeClone(that.mapping[heroName].sourceClone, that.mapping[heroName].sourceBoundingClientRect);
+                    }
 
                 });
             };
+        },
+
+        _transit: function() {
+
+            for(var heroName in this.mapping) {
+                console.log('transit hero', heroName)
+                var hero = this.mapping[heroName];
+
+                if(hero.source && hero.dest)
+                    this._transitHero(hero, heroName);
+            }
+        },
+
+        _transitHero: function(hero) {
+            var sourceClone = hero.sourceClone;
+            var dest = hero.dest;
+            var destClone = hero.destClone;
+
+            dest.style.opacity = 0;
+            destClone.style.opacity = 0;
+            destClone.style.transition = 'all .3s ease';
+
+            sourceClone.style.transition = 'all .3s ease';
+            sourceClone.style.cssText = getComputedStyle(destClone).cssText;
+            sourceClone.style.opacity = 1;
+
+            var transitionEndHandled = false;
+            sourceClone.addEventListener('transitionend', function() {
+                if(transitionEndHandled)
+                    return;
+
+                transitionEndHandled = true;
+                sourceClone.remove();
+                destClone.remove();
+                dest.style.opacity = 1;
+            });
+        },
+
+        _placeClone: function(clone, boundingClientRect) {
+            var rect = boundingClientRect;
+
+            clone.style.position = 'fixed';
+            clone.style.top = rect.top + 'px';
+            clone.style.left = rect.left + 'px';
+            clone.style.width = rect.width + 'px';
+            clone.style.height = rect.height + 'px';
+
+            document.body.appendChild(clone);
+
+            return clone;
         }
     };
 
 
 
+    VueHero._instances = {};
     /**
      *
      * @param {Object} options - {fade: ['hero-one', 'hero-two'], expand: ['hero-one', 'hero-three']}
@@ -71,29 +162,15 @@
 
         for(var transitionName in options) {
             var heroNames = options[transitionName];
-            var transitionOptions = Vue.options.transitions[transitionName];
+            var transitionOptions = Vue.options.transitions[transitionName] || {};
 
-            new VueHero({
+            VueHero._instances[transitionName] = new VueHero({
                 transitionName: transitionName,
                 transitionOptions: transitionOptions,
                 heroNames: heroNames
             });
         }
     };
-
-
-    function _getHeroElements(heroName) {
-        return document.querySelectorAll('[hero-name="' + heroId + '"]');
-    }
-
-    function _cloneHero(heroElement) {
-        var clonedHero = heroElement.cloneNode();
-        var rect = heroElement.getBoundingClientRect();
-
-        clonedHero.style.position = 'fixed';
-
-        return clonedHero;
-    }
 
     return VueHero
 }));
